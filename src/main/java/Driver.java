e07db628f3531a532182bd684a915cd21d6a7bba
James Churchill <james@smashdev.com>
2020-10-13 13:04:23 -0700

    Update Driver class

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,5 +2,8 @@ public class Driver {
 
     public static void main(String[] args) {
         System.out.println();
+
+        System.out.println("Why do java programmers wear glasses?");
+        System.out.println("Because they can’t C#");
     }
 }
